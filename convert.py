895637f1a7a893a6b64ea9c85f93d992ae535596
Dan Moseley <danomoseley@gmail.com>
2013-09-02 21:23:48 -0400

    Remove empty line

--- convert.py
+++ convert.py
@@ -6,7 +6,6 @@ import Tkinter
 from tkFileDialog import askopenfilename
 IS_WIN = platform.system().lower() == 'windows'
 
-
 print 'Checking for new version'
 p = subprocess.Popen(["git", "pull"], shell=False, stdout=subprocess.PIPE)
 out, err = p.communicate()
