20b9bbdb4bdf58d1f0dfa64fb37e75ea85cb9117
Dan <Dan@jooraccess.com>
2013-09-15 10:58:48 -0400

    Output single solid solutions xf1

--- convert.py
+++ convert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, csv, time, os, pprint, traceback, collections, platform
+import sys, csv, time, os, traceback, collections, platform
 import subprocess
 from Tkinter import Tk
 import Tkinter
@@ -195,13 +195,12 @@ def convert_ingredient_list():
         raw_input("Press enter to quit")
         sys.exit()
 
-def convert_to_xf1(product_code_prefix, plant_number, plant_name):
+def convert_to_xf1(plants):
     try:
-        prompt = "SS ingredient cost file for %s (%s / %s)" % (plant_name, plant_number, product_code_prefix)
-        input_filename = get_required_file(prompt)
-        #input_filename = 'BRANDON TEST MO 09.02.13.TXT'
-
-        output_filename = directory+os.sep+'Master%sIng%s.xf1' % (formated_date, product_code_prefix)
+        plant_product_codes = {}
+        output_filename = directory+os.sep+'Master%sIng.xf1' % formated_date
+        f = open(output_filename,'wb')
+        f.write('XF Version = 5\r\n')
 
         prefix_exclusions = {}
         with open('exclusions.txt', 'rb') as exclusionsfile:
@@ -210,24 +209,32 @@ def convert_to_xf1(product_code_prefix, plant_number, plant_name):
                 if row[0]:
                     prefix_exclusions[row[0]] = True
 
-        product_codes_sorted = []
-        product_codes = {}
-        with open(input_filename, 'rb') as csvfile:
-            f = open(output_filename,'wb')
-            reader = csv.reader(csvfile, delimiter='\t', quotechar='"')
-            f.write('XF Version = 5\r\n')
-            next(reader)
-            next(reader)
-            plant = next(reader)
+        for plant in plants:
+            reader = None
+            valid_input_file = False
+            while not valid_input_file:
+                prompt = "SS ingredient cost file for %s (%s / %s)" % (plant['plant_name'], plant['plant_number'], plant['product_code_prefix'])
+                input_filename = get_required_file(prompt)
+                csvfile = open(input_filename, 'rb')
+                reader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+                next(reader)
+                next(reader)
+                plant_name_line = next(reader)
+                input_file_plant_name = plant_name_line[0]
+                if input_file_plant_name.lower() != plant['plant_name'].lower():
+                    print "\r\n%s is for %s, %s expected" % (input_filename, input_file_plant_name, plant['plant_name'])
+                    prompt = "Try again for %s? [y/n]: " % plant['plant_name']
+                    user_input = raw_input(prompt)
+                    if user_input.lower() == 'n':
+                        continue
+                else:
+                    valid_input_file = True
 
-            if plant and plant[0].lower() != plant_name.lower():
-                print "\r\n%s is for %s, %s expected" % (input_filename, plant[0], plant_name)
-                convert_to_xf1(product_code_prefix, plant_number, plant_name)
-                return False
-
             next(reader)
             next(reader)
             next(reader)
+            product_codes_sorted = []
+            product_codes = {}
             for row in reader:
                 if row:
                     if row[0] == 'Product' or row[0] == 'Code':
@@ -236,7 +243,7 @@ def convert_to_xf1(product_code_prefix, plant_number, plant_name):
                     product_code = row[0].strip()
                     product_code = product_code.zfill(4)
                     if product_code not in prefix_exclusions:
-                        product_code = product_code_prefix + product_code
+                        product_code = plant['product_code_prefix'] + product_code
                     else:
                         print "Skipping prefix for %s" % product_code
                     product_codes[product_code] = True
@@ -247,9 +254,15 @@ def convert_to_xf1(product_code_prefix, plant_number, plant_name):
                     f.write(price.rjust(12))
                     f.write(product_code.rjust(128))
                     f.write('\r\n')
+
             product_codes_sorted.sort()
+            plant_product_codes[plant['plant_name']] = {
+                'product_codes': product_codes,
+                'product_codes_sorted': product_codes_sorted,
+            }
+
         if IS_WIN:
-            ingredient_selection(product_codes, product_codes_sorted)
+            ingredient_selection(plant_product_codes)
         return True
     except Exception, e:
         print "Error encountered in convert_to_xf1: " + str(e)
@@ -265,32 +278,34 @@ def copy_and_get_clipboard_data():
     win32clipboard.CloseClipboard()
     return data
 
-def ingredient_selection(product_codes, product_codes_sorted):
+def ingredient_selection(plant_product_codes):
     try:
-        print "You will have 5 seconds to select the code column of a row on the export screen."
-        user_input = raw_input("Press enter when ready for ingredient selection, or n to skip: ").lower()
+        for plant_name in plant_product_codes:
+            print "You will have 5 seconds to select the code column of a row on the export screen."
+            prompt = "Press enter when ready for ingredient selection for %s, or n to skip: " % plant_name
+            user_input = raw_input(prompt)
 
-        if user_input != 'n':
-            time.sleep(5)
+            if user_input.lower() != 'n':
+                time.sleep(5)
 
-            last_data = ''
-            data = copy_and_get_clipboard_data()
-            shell.SendKeys("{DOWN}", 0)
-            max_product_code = product_codes_sorted[-1]
-
-            found = 0
-            while data < max_product_code and data != last_data:
-                last_data = data
+                last_data = ''
                 data = copy_and_get_clipboard_data()
-                if data in product_codes:
-                    print "%s found" % data
-                    shell.SendKeys("{LEFT}", 0)
-                    shell.SendKeys(" ", 0)
-                    shell.SendKeys("{RIGHT}", 0)
-                    found = found + 1
                 shell.SendKeys("{DOWN}", 0)
-            print "%d codes in file" % len(product_codes_sorted)
-            print "%d codes found and selected" % found
+                max_product_code = plant_product_codes[plant_name]['product_codes_sorted'][-1]
+
+                found = 0
+                while data < max_product_code and data != last_data:
+                    last_data = data
+                    data = copy_and_get_clipboard_data()
+                    if data in plant_product_codes[plant_name]['product_codes']:
+                        print "%s found" % data
+                        shell.SendKeys("{LEFT}", 0)
+                        shell.SendKeys(" ", 0)
+                        shell.SendKeys("{RIGHT}", 0)
+                        found = found + 1
+                    shell.SendKeys("{DOWN}", 0)
+                print "%d codes in file" % len(plant_product_codes[plant_name]['product_codes_sorted'])
+                print "%d codes found and selected" % found
     except Exception, e:
         print "Error encountered in ingredient_selection: " + str(e)
         print traceback.format_exc()
@@ -305,9 +320,11 @@ else:
     process_ss = 'y'
 
 if process_ss.lower() in ['y','']:
-    convert_to_xf1('61', '560', 'Augusta Mill')
-    convert_to_xf1('64', '550', 'Adams Center')
-    convert_to_xf1('66', '580', 'Sangerfield Mill')
-    convert_to_xf1('68', '570', 'Brandon Mill')
+    convert_to_xf1([
+        {'product_code_prefix': '61', 'plant_number': '560', 'plant_name': 'Augusta Mill'},
+        {'product_code_prefix': '64', 'plant_number': '550', 'plant_name': 'Adams Center'},
+        {'product_code_prefix': '66', 'plant_number': '580', 'plant_name': 'Sangerfield Mill'},
+        {'product_code_prefix': '68', 'plant_number': '570', 'plant_name': 'Brandon Mill'}
+    ])
 
 raw_input("Process complete. Press enter to quit")
